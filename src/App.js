fde47a576b7e3f071f04237242855d87d0c103f6
Shubham Verma <lpushubham98@gmail.com>
2021-06-09 10:06:05 +0530

    tds

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "react-bootstrap";
 import Dialog from "./components/Dialog";
 import Cards from "./components/Cards";
 
